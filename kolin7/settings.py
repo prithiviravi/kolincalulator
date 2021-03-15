5fab0906868b7b3e5ebc7649a55dbc74ffbd42b6
unknown <prithiviravi7@gmail.com>
2021-03-15 23:38:41 +0530

    updated

--- kolin7/settings.py
+++ kolin7/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_psohy3#+a6%0yp5#0&t6wdt^n^zrf&4m=8b*)na#_1@@#337o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','kolinplusmatrixcalculator.herokuapp.com']
 
 
 # Application definition
