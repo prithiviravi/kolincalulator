447dc548714cd1925f81fa228072e9e6e1e84041
unknown <prithiviravi7@gmail.com>
2021-03-16 00:15:20 +0530

    updated

--- kolin7/settings.py
+++ kolin7/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_psohy3#+a6%0yp5#0&t6wdt^n^zrf&4m=8b*)na#_1@@#337o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = ['127.0.0.1','kolinplusmatrixcalculator.herokuapp.com']
 
