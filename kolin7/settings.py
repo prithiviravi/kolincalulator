964b0aff19c4fb5e91c245c44c2b00fb8bc31859
unknown <prithiviravi7@gmail.com>
2021-03-16 10:43:50 +0530

    'updated'

--- kolin7/settings.py
+++ kolin7/settings.py
@@ -124,5 +124,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT= os.path.join(BASE_DIR, "assets")
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
