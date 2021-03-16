ed5cb432065e086549fc038f0f1fbd662fa0c083
unknown <prithiviravi7@gmail.com>
2021-03-16 10:45:22 +0530

    'updated'

--- kolin7/settings.py
+++ kolin7/settings.py
@@ -76,12 +76,6 @@ WSGI_APPLICATION = 'kolin7.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
