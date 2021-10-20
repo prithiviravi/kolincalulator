dedd3df65de5d4decb811c9807257d68ab2937eb
prithivirajan.r <prithivirajan.r@aspirantlabs.com>
2021-10-20 23:32:58 +0530

    first commit

--- kolin11/settings.py
+++ kolin11/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_psohy3#+a6%0yp5#0&t6wdt^n^zrf&4m=8b*)na#_1@@#337o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
 
 ALLOWED_HOSTS = ['127.0.0.1','kolinplusmatrixcalculator.herokuapp.com','kolinplusmatrixcalcus.herokuapp.com','kolinpluscalcus.herokuapp.com']
